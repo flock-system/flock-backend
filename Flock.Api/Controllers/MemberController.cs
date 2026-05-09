800b694abd013142a31f8f504adce6120d4d2475
Rafael <rafaelfuzari@gmail.com>
2026-05-09 19:40:20 -0300

    feat: member class, member controller class

---
+++ Flock.Api/Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Flock.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MemberController : ControllerBase
+    {
+
+        [HttpPost("create")]
+        public IActionResult CreateMember()
+        {
+            string name = "joão";
+
+            return Ok(name);
+        }
+
+    }
+}
---
+++ Flock.Domain/Entities/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flock.Domain.Entities
+{
+    internal class Member
+    {
+        public int id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string age { get; set; }
+
+
+    }
+}
