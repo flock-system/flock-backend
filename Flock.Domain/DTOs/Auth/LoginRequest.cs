441fd9d359c888608393ba9882c74f4a3dcf27e4
Rafael <rafaelfuzari@gmail.com>
2026-05-25 09:13:16 -0300

    feat: created login request DTO

---
+++ Flock.Domain/DTOs/Auth/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flock.Domain.DTOs.Auth
+{
+    public class LoginRequest
+    {
+        public string username {  get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+
+    }
+}
